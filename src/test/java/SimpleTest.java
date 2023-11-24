c868089dd14e97d6f74c94b9a4cdf763fc8f325a
Joris <mathijsen.joris@gmail.com>
2023-11-24 22:46:54 +0100

    Test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,5 +1,7 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.example.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleTest {
 
